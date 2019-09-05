9ea3689b29ce3144436c5104a1777761132ee1dd
BarcaBro1022 <you.k2208@gmail.com>
2019-09-04 21:13:54 -0700

    created array and tracking variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+const dogs = [`newfoundland`, `xoloitzcuintli`, `pug`, `bulldog`, `chihuahua`, `pitbull`, `german shephard`, `labrador retriever`, `chow chow`, 'bernese mountain dog', `beagle`, `poodle`, `french bulldog`, `husky`, `doberman`, `golden retriever`, `great dane`, `pomeranian`, `corgi`, `maltese`, `boston terrier`, `rottweiler`, `yorkshire terrier`, `bloodhound`, `saint bernard`, `akita`, `shiba inu`, `mastiff`, `dachshund`]
+
+let wins = 0
+let losses = 0
+let guesses = 3
+const lettersGuessed = []
+
+const displayWord = function () {
+  
+}
