3020f98a8421c2714f470d62b6cc90b0112493bd
BarcaBro1022 <you.k2208@gmail.com>
2019-09-04 21:43:34 -0700

    rearranged logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,21 @@
+// dog array
 const dogs = [`newfoundland`, `xoloitzcuintli`, `pug`, `bulldog`, `chihuahua`, `pitbull`, `german shephard`, `labrador retriever`, `chow chow`, 'bernese mountain dog', `beagle`, `poodle`, `french bulldog`, `husky`, `doberman`, `golden retriever`, `great dane`, `pomeranian`, `corgi`, `maltese`, `boston terrier`, `rottweiler`, `yorkshire terrier`, `bloodhound`, `saint bernard`, `akita`, `shiba inu`, `mastiff`, `dachshund`]
 
+// select random dog
+const getRandDog = function () {
+  return dogs[Math.floor(Math.random() * dogs.length)]
+}
+
+// variables
 let wins = 0
 let losses = 0
 let guesses = 3
 const lettersGuessed = []
+let dog = getRandDog()
 
 
-// select random word and display word
-const getRandDog = function () {
-  return dogs[Math.floor(Math.random() * dogs.length)]
-}
-const displayDog = function () {
-  const dog = getRandDog()
+// display dog
+const displayDog = function () {  
   let dogStr = ``
   dog.split(``).forEach(function (letter) {
     dogStr += `_ `
@@ -21,4 +25,9 @@ const displayDog = function () {
 
 }
 
-displayDog()
+// get player input
+document.onkeyup() = function (event) {
+  if (event.keycode >= 65 && <= 90) {
+    
+  }
+}
